a6036a884bd1ba1b277fc342aebadaa9ccf52dd9
Damianmilczarek5 <damian.milczarek2000@gmail.com>
2022-03-02 19:46:10 +0100

    added 2 functions that renders one city and whole array of cities into HTML

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ function addCityToDatabase(database, city) {
     // "pushes" a new city to our database
     database.push(city);
 }
+
+// renders a city into HTML 
+function renderCity(city) {
+    let div = document.createElement("div");
+    div.classList.add("city");
+    div.id = city.id;
+
+    div.innerHTML = `
+        <div>${city.name}</div>
+        <div>${city.population}</div>
+        <div>${city.county}</div>
+        <div>${city.attraction}</div>
+        <button type="button">Remove</button>
+    `;
+
+  return div;
+}
+
+// renders a whole array of cities onto HTML 
+function renderCities(cities) {
+    let citiesElement = document.getElementById("cities");
+  citiesElement.innerHTML = "";
+
+  // made a for loop that goes throught all cities and inserts them into HTML 
+  for (let city of cities) {
+    let cityElement = rendercity(city);
+    citiesElement.appendChild(cityElement);
+  }
+  // Add remove-handlers for our cities
+  setRemovecityHandlers();
+}
