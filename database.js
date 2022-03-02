160e58654996973a7ba7846e69abe1cb574d27cc
Damianmilczarek5 <damian.milczarek2000@gmail.com>
2022-03-02 19:24:05 +0100

    made a seperate js file with my database inside

---
+++ database.js
@@ -0,0 +1,31 @@
+let database = [
+    {
+      id: 1,
+      name: "Stockholm",
+      population: 975000,
+      county: "Stockholm",
+      attraction: "Avicii Arena",
+    },
+    {
+      id: 2,
+      name: "Malmö",
+      population: 350000,
+      county: "Skåne",
+      attraction: "Turning torso",
+    },
+    {
+      id: 3,
+      name: "Göteborg",
+      population: 580000,
+      county: "Hallands",
+      attraction: "Liseberg",
+    },
+    {
+      id: 4,
+      name: "Helsinborg",
+      population: 120000,
+      county: "Skåne",
+      attraction: "Kärnan",
+    },
+  ];
+  
